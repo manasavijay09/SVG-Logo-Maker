457c9f29ccf95cb7625a6607c112d630ab8aad91
manasavijay09 <manasa.vijay09@gmail.com>
2024-01-17 21:57:42 -0600

    added readme file discription

--- index.js
+++ index.js
@@ -1,40 +1,67 @@
 const inquirer = require("inquirer");
-const fs = require('fs');
-const generateLogo= require('./generateLogo.js');
+const fs = require("fs");
+const generateLogo = require("./generateLogo.js");
+const Circle = require("./lib/circle.js");
+const Square = require("./lib/Square.js");
+const Triangle = require("./lib/triangle.js");
+const Svg=require('./lib/Svglogo.js');
 
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Please Enter three charcters or three letters ",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter a text color keyword (OR a hexadecimal number)",
+      },
+      {
+        type: "list",
+        name: "Shape",
+        message: "Enter the shape",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a shape color keyword (OR a hexadecimal number)",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      switch (response.Shape) {
+        case "Circle":
+          const mycircle = new Circle(response.shapeColor);
+          return { render: mycircle.render(), response: response };
+          
+        case "Square":
+          const myLogo2 = new Square(response.shapeColor);
+          return { render: myLogo2.render(), response: response };
 
-function init(){
-    inquirer.prompt([
-        {
-           type : "input",
-           name:"text",
-           message:"Please Enter three charcters or three letters "
-        },
-        {
-            type : "input",
-            name:"color",
-            message:"Enter a text color keyword (OR a hexadecimal number)"
-         },
-         {
-            type : "list",
-            name:"Shape",
-            message:"Enter the shape",
-            choices:["Circle","Triangle","Square",]
-         },
-         {
-            type : "input",
-            name:"color",
-            message:"Enter a shape color keyword (OR a hexadecimal number)"
-         },
-        
-         
-    ]).then(response => {
-        console.log(response)
-        fs.writeFileSync("Generated logo.svg", generateLogo(response),function(err,data){
-            if(err) throw err
-        })
-        console.log("svglogomaker");
+        case "Triangle":
+          const myLogo3 = new Triangle(response.shapeColor);
+          return { render: myLogo3.render(), response: response };
+      }
     })
-    }
-    
-    init()
+    .then((result) => {
+        console.log(result)
+        const mylogo=new Svg();
+        mylogo.setText(result.response.text,result.response.textColor)
+        mylogo.setshapeElement(result.render)
+        return mylogo.render()
+
+    }).then((svgText) => {
+        console.log(svgText)
+      fs.writeFileSync("Generatedlogo.svg",svgText,function (err, data) {
+          if (err) throw err;
+        }
+      );
+      console.log("svglogomaker");
+    });
+}
+
+init();
